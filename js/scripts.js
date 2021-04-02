d398becc5c155f2a590336e9ecb44794d4694549
Maxwell Meyer <maxreadswell@gmail.com>
2021-04-02 14:29:34 -0700

    Add scripts to return correct numbers/replacements

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,32 @@
-for (let i=0; i<=X; i ++) {
+function mrRoboto(N) {
+  let rogersArray = [];
+  let neighborhood = [];
+  let newNeighbor = " ";
+  for (let i=0; i <= N; i ++) {
+    rogersArray.push(i.toString());
+    if (rogersArray[i].includes("3")) {
+      neighborhood.push("Won't you be my neighbor?");
+    }
+    else if (rogersArray[i].includes("2")) {
+      neighborhood.push("Boop! ");
+    }
+    else if (rogersArray[i].includes("1")) {
+      neighborhood.push("Beep! ");
+    }
+    else {
+      neighborhood.push(rogersArray[i]+ ",");
+    }      
+  } 
+  return neighborhood
 }
-
-
-
-
-$(document).ready(function() {
-  $('#rogerForm').submit(function(event) {
+$(document).ready(function(){
+  $("form#rogerForm").submit(function(event){
     event.preventDefault();
-    let 
-  })
-})
+    let newNeighbor = $("input#username").val();
+    let X =$("input#numb").val();
+    let newSweater = mrRoboto(X);
+    $("#mrRogers").text(newSweater);
+    $("#newNeighbor").text(newNeighbor);
+      $("#results").show();
+  });
+});
